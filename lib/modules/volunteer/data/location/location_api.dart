16f896d923ccb917a72a6f2503ae13a20a4fdcb8
Islam Ahmed <62517820+Hakugami@users.noreply.github.com>
2023-06-25 00:01:40 +0300

    changed location_api.dart

--- lib/modules/volunteer/data/location/location_api.dart
+++ lib/modules/volunteer/data/location/location_api.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-import 'package:background_location/background_location.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:location/location.dart' as loc;
-import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart' as loc;
 import 'package:object_detection/modules/volunteer/data/firebase/user_firebase.dart';
 import 'package:object_detection/modules/volunteer/ui/volunteer_request/cubit/cubit.dart';
 import 'package:object_detection/shared/constants.dart';
@@ -11,7 +8,7 @@ import '../../../../models/Request.dart';
 import '../../../../models/User.dart';
 
 class LocationApi {
-  static loc.Location _location = new loc.Location();
+  static loc.Geolocator _geolocator = loc.Geolocator();
 
   static late VolunteerRequestCubit myCubit;
 
@@ -19,71 +16,57 @@ class LocationApi {
       VolunteerRequestCubit cubit) async {
     if (await _checkServiceAvailability() && await _checkLocationPermission()) {
       myCubit = cubit;
-      BackgroundLocation.setAndroidNotification(
-          title: "Blind Assistant",
-          message: "listen continuous location changes",
-          icon: "@mipmap/ic_launcher");
-      // listen on location change every 3 minutes
-      BackgroundLocation.setAndroidConfiguration(1000 * 60 * 3);
-      BackgroundLocation.startLocationService();
-      // listen on location change every 3 minutes
-      _listenOnLocationChange();
-      // showToast('Request is sent successfully');
+      loc.Geolocator.getPositionStream(
+        desiredAccuracy: loc.LocationAccuracy.best,
+        intervalDuration: Duration(minutes: 3),
+      ).listen((loc.Position position) async {
+        await _updateUserLocation(position.latitude, position.longitude);
+      });
     }
     myCubit = cubit;
   }
 
   static Future<bool> _checkServiceAvailability() async {
-    bool _serviceEnabled = await _location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await _location.requestService();
+    bool serviceEnabled = await loc.Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Handle the failure to enable location service
     }
-    return _serviceEnabled;
+    return serviceEnabled;
   }
 
   static Future<bool> _checkLocationPermission() async {
-    PermissionStatus _permissionGranted = await _location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await _location.requestPermission();
+    loc.LocationPermission permission =
+    await loc.Geolocator.checkPermission();
+    if (permission == loc.LocationPermission.denied) {
+      permission = await loc.Geolocator.requestPermission();
+      if (permission != loc.LocationPermission.whileInUse &&
+          permission != loc.LocationPermission.always) {
+        // Handle the failure to grant location permission
+      }
     }
-    return _permissionGranted == PermissionStatus.granted;
-  }
-
-  static StreamSubscription<loc.LocationData>? _locationSubsrcibtion;
-
-  static void _listenOnLocationChange() async {
-    /* await _location.changeSettings(distanceFilter: 10);
-    _locationSubsrcibtion = _location.onLocationChanged.handleError((onError) {
-      _locationSubsrcibtion!.cancel();
-      _locationSubsrcibtion = null;
-    }).listen((locationData) async {
-      await _updateUserLocation(locationData.latitude, locationData.longitude);
-    });*/
-    try {
-      BackgroundLocation.getLocationUpdates((location) async {
-        await _updateUserLocation(location.latitude, location.longitude);
-      });
-    } catch (err) {}
+    return permission == loc.LocationPermission.whileInUse ||
+        permission == loc.LocationPermission.always;
   }
 
   static Request? request;
 
-  static _updateRequestObj(double latitude, double longitude) async {
+  static void _updateRequestObj(double latitude, double longitude) async {
     UserModel userModel = await getCurrentUser();
-    if (request == null)
+    if (request == null) {
       request = Request(
-          blindData: userModel,
-          blindLocation: UserLocation(latitude, longitude),
-          date: Timestamp.now());
-    else {
+        blindData: userModel,
+        blindLocation: UserLocation(latitude, longitude),
+        date: Timestamp.now(),
+      );
+    } else {
       request!.blindLocation.latitude = latitude;
       request!.blindLocation.longitude = longitude;
     }
   }
 
   static Future<void> _updateUserLocation(
-      double? latitude, double? longitude) async {
-    _updateRequestObj(latitude!, longitude!);
+      double latitude, double longitude) async {
+    _updateRequestObj(latitude, longitude);
     try {
       await UserFirebase.setRequestForAllVolunteers(request!, myCubit);
       //requestState = RequestSucceeded();
